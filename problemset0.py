1f51b86285b76da920583cb15ec58df7c8208e19
ruhanir-dot <ruhanirekhi@gmail.com>
2025-07-04 11:59:17 -0700

    Add files via upload
    Solution to Problem Set 0 with comments

---
+++ problemset0.py
@@ -0,0 +1,19 @@
+"""
+Write a program that does the following in order:
+1. Asks the user to enter a number “x”
+2. Asks the user to enter a number “y”
+3. Prints out number “x”, raised to the power “y”.
+4. Prints out the log (base 2) of “x”.
+"""
+import math
+
+# PART 1 & 2
+
+# taking in user input x and y
+x = int(input("Enter a number:"))
+y = int(input("Enter another number: "))
+
+# PART 3 & 4
+print(x ** y)
+print(math.log(x,2))
+
